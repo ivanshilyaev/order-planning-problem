d91c00da0a8e30ede0a5296fadb9cc5fd569e279
Ivan Shilyaev <shilyaev1433@gmail.com>
2021-08-20 13:16:43 +0300

    Initial commit

---
+++ src/main/java/com/example/orderplanning/OrderPlanningProblemApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPlanningProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderPlanningProblemApplication.class, args);
+    }
+
+}
